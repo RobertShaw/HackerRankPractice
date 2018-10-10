c6e55665701df15160c1bc7c76f68571a2d0caa1
RobertShaw <robleeshaw@gmail.com>
2018-10-10 13:20:35 -0700

    Create Mark and Toys.py

---
+++ Practice/Interview Preparation Kit/Sorting/Mark and Toys.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    count = 0
+    while k > 0 and count < len(prices):
+        k = k - prices[count]
+        count = count + 1
+    if k < 0:
+        count = count - 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
