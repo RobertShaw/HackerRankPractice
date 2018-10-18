ed71c349d875a78e03010f7de4478445b1e67f3e
RobertShaw <robleeshaw@gmail.com>
2018-10-18 13:22:17 -0700

    Create Sock Merchant.py

---
+++ Practice/Interview Preparation Kit/Warm Up/Sock Merchant.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    numSocks = 0
+    count = Counter(ar)
+
+    for key,value in dict(count).items():
+        numSocks = numSocks + value//2
+        
+    return numSocks
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
